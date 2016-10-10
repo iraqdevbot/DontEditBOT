95a04cd19873b6022c5c6a7bd834cc5fe4836ff5
Wathiq Al-Qajar <wathiq-iq@users.noreply.github.com>
2016-10-11 02:42:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -50,11 +50,11 @@ if (isset($update->edited_message)){
     'text'=>$text,
     'disable_web_page_preview'=>'true',
     'parse_mode'=>'html',
-    'reply_markup'=>json_encode([
-      'inline_keyboard'=>[
-        [
-          ['text'=>'Channel','url'=>'https://telegram.me/joinchat/DtebJD-YicabzaggOWIHeQ']
-        ]
+    'reply_markup'=>['keyboard'=>[
+      [
+      ['text'=>'/submit']
+  ]
+       ]
       ]
     ])
   ]);
